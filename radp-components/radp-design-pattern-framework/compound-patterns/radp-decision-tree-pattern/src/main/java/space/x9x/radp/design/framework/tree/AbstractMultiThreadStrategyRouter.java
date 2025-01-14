95b3f12f2654361e2b63716f3ca92224fe55abb0
xooooooooox <xozoz.sos@gmail.com>
2025-01-14 21:58:06 +0800

    feat: 决策树设计模式,增加多线程异步数据加载

---
+++ radp-components/radp-design-pattern-framework/compound-patterns/radp-decision-tree-pattern/src/main/java/space/x9x/radp/design/framework/tree/AbstractMultiThreadStrategyRouter.java
@@ -0,0 +1,35 @@
+package space.x9x.radp.design.framework.tree;
+
+/**
+ * 多线程异步资源加载 + 策略路由的抽象类
+ *
+ * @author x9x
+ * @since 2025-01-14 21:53
+ */
+public abstract class AbstractMultiThreadStrategyRouter<T, D, R> extends AbstractStrategyRouter<T, D, R> {
+
+    @Override
+    public R apply(T requestParameter, D dynamicContext) throws Exception {
+        // 异步加载数据
+        multiThread(requestParameter, dynamicContext);
+        // 业务流程受理
+        return doApply(requestParameter, dynamicContext);
+    }
+
+    /**
+     * 异步加载数据
+     *
+     * @param requestParameter 入参
+     * @param dynamicContext   动态上下文
+     */
+    protected abstract void multiThread(T requestParameter, D dynamicContext);
+
+    /**
+     * 业务流程受理
+     *
+     * @param requestParameter 入参
+     * @param dynamicContext   动态上下文
+     * @return 执行结果
+     */
+    protected abstract R doApply(T requestParameter, D dynamicContext);
+}

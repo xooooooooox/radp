bd796998d2589141f2cbc5f29e1dab710e4c8086
xooooooooox <xozoz.sos@gmail.com>
2025-02-08 12:12:27 +0800

    feat: optimize archetype docker image build

--- radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/start.sh
+++ radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/start.sh
@@ -2,14 +2,14 @@
 
 # 检查容器是否已存在
 if docker container inspect ${appName} &>/dev/null; then
-    read -p "容器 '${appName}' 已存在，是否需要删除它？(y/n): " choice
-    if [[ "$choice" =~ ^[Yy]$ ]]; then
-        echo "正在删除容器 'chatgpt-api'..."
-        docker rm -f chatgpt-api
-    else
-        echo "操作中止。"
-        exit 0
-    fi
+  read -p "容器 '${appName}' 已存在，是否需要删除它？(y/n): " choice
+  if [[ "$choice" =~ ^[Yy]$ ]]; then
+    echo "正在删除容器 'chatgpt-api'..."
+    docker rm -f chatgpt-api
+  else
+    echo "操作中止。"
+    exit 0
+  fi
 fi
 
 # 启动新容器

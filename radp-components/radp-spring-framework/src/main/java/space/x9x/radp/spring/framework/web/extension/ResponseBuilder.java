8524ad01e4a4352d495b45e358b511b911cde454
xooooooooox <xozoz.sos@gmail.com>
2025-03-28 03:28:09 +0800

    fix(radp): fix `radp-spring-framework` ResponseBuilder bug

--- radp-components/radp-spring-framework/src/main/java/space/x9x/radp/spring/framework/web/extension/ResponseBuilder.java
+++ radp-components/radp-spring-framework/src/main/java/space/x9x/radp/spring/framework/web/extension/ResponseBuilder.java
@@ -1,9 +1,11 @@
 package space.x9x.radp.spring.framework.web.extension;
 
+import org.jetbrains.annotations.PropertyKey;
 import space.x9x.radp.extension.ExtensionLoader;
 import space.x9x.radp.extension.SPI;
+import space.x9x.radp.spring.framework.beans.ApplicationContextHelper;
+import space.x9x.radp.spring.framework.error.ErrorCode;
 import space.x9x.radp.spring.framework.error.ErrorCodeLoader;
-import org.jetbrains.annotations.PropertyKey;
 
 /**
  * @author x9x
@@ -13,6 +15,10 @@ import org.jetbrains.annotations.PropertyKey;
 public interface ResponseBuilder<T> {
 
     static ResponseBuilder<?> builder() {
+        ResponseBuilder<?> builder = ApplicationContextHelper.getBean(ResponseBuilder.class);
+        if (builder != null) {
+            return builder;
+        }
         return ExtensionLoader.getExtensionLoader(ResponseBuilder.class).getDefaultExtension();
     }
 
@@ -24,4 +30,6 @@ public interface ResponseBuilder<T> {
     T buildFailure(@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... params);
 
     T buildFailure(@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, String errMessage, Object... params);
+
+    T buildFailure(ErrorCode errorCode);
 }

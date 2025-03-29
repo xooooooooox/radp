5c4361bd23632403e48d19fc1f0251b29bf650dc
xooooooooox <xozoz.sos@gmail.com>
2025-03-29 20:22:35 +0800

    feat(radp): Disable autoconfigure `BootstrapLogAutoConfiguration`, `AccessLogAutoConfiguration`, `WebAPIAutoConfiguration`

--- radp-components/radp-spring-boot/src/main/java/space/x9x/radp/spring/boot/web/autoconfigure/WebAPIAutoConfiguration.java
+++ radp-components/radp-spring-boot/src/main/java/space/x9x/radp/spring/boot/web/autoconfigure/WebAPIAutoConfiguration.java
@@ -1,15 +1,15 @@
 package space.x9x.radp.spring.boot.web.autoconfigure;
 
-import space.x9x.radp.spring.boot.web.env.WebAPIProperties;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Role;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import space.x9x.radp.spring.boot.web.env.WebAPIProperties;
 
 /**
  * @author x9x
@@ -17,13 +17,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @AutoConfiguration
 @EnableConfigurationProperties(WebAPIProperties.class)
+@ConditionalOnClass(WebMvcConfigurer.class)
 @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-@RequiredArgsConstructor
 @Slf4j
 public class WebAPIAutoConfiguration implements WebMvcConfigurer {
 
 
     private final WebAPIProperties properties;
+
+    public WebAPIAutoConfiguration(WebAPIProperties properties) {
+        log.debug("Autowired webApiConfiguration");
+        this.properties = properties;
+    }
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {

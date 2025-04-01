fb1ff79c75b6f9d40efe3a51e79d007e20321914
xooooooooox <xozoz.sos@gmail.com>
2025-04-01 13:04:42 +0800

    feat(radp): `radp-common` add `FileUtils`

---
+++ radp-components/radp-commons/src/main/java/space/x9x/radp/commons/io/FileUtils.java
@@ -0,0 +1,11 @@
+package space.x9x.radp.commons.io;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author x9x
+ * @since 2025-03-31 18:41
+ */
+@UtilityClass
+public class FileUtils extends org.apache.commons.io.FileUtils {
+}

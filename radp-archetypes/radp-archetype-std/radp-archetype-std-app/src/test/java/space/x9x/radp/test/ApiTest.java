3afeaaf8a6446f31d09177ce1f613f750e5c92e0
xooooooooox <xozoz.sos@gmail.com>
2025-04-09 16:11:30 +0800

    feat(scaffold): Delete `JasyptTest`, Add blank Junit5 `ApiTest`

---
+++ radp-archetypes/radp-archetype-std/radp-archetype-std-app/src/test/java/space/x9x/radp/test/ApiTest.java
@@ -0,0 +1,17 @@
+package space.x9x.radp.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author x9x
+ * @since 2025-04-09 12:34
+ */
+@SpringBootTest
+class ApiTest {
+
+    @Test
+    void test() {
+
+    }
+}

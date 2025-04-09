0ba7382a85d6eab8e5339fd48ad18f3b46707cf6
xooooooooox <xozoz.sos@gmail.com>
2025-04-09 12:49:27 +0800

    feat(radp): `radp-jasypt-spring-boot-starter` add Test

---
+++ radp-components/radp-spring-boot-starters/radp-jasypt-spring-boot-starter/src/test/java/space/x9x/radp/jasypt/spring/boot/TestApplication.java
@@ -0,0 +1,11 @@
+package space.x9x.radp.jasypt.spring.boot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author x9x
+ * @since 2025-04-09 11:46
+ */
+@SpringBootApplication
+public class TestApplication {
+}

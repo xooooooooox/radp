b878c6d01e5f1da01aab528b235484def7823a0f
xooooooooox <xozoz.sos@gmail.com>
2025-04-17 23:38:11 +0800

    fix(scaffold): Fix build.sh and start.sh

--- radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-app/build.sh
+++ radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-app/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
 
 pushd .. || exit 1
 ./mvnw clean -Dauto.layered.enabled=true -Pcoding,env-dev package || sh mvnw clean -Dauto.layered.enabled=true -Pcoding,env-dev package
 popd || exit 1
 docker build -f ./Dockerfile -t ${imageNamespace}/${appName} .
-docker push ${imageNamespace}/${appName} .
+docker push ${imageNamespace}/${appName}
--- radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/build.sh
+++ radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+set -e
 
 ./mvnw clean -Dauto.layered.enabled=true -Pcoding,env-dev package || sh mvnw clean -Dauto.layered.enabled=true -Pcoding,env-dev package
 docker build -f ./Dockerfile -t ${imageNamespace}/${appName} .
-docker push ${imageNamespace}/${appName} .
+docker push ${imageNamespace}/${appName}

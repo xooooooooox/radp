775bd9b277002134e784d706a983e47d5a35614f
xooooooooox <xozoz.sos@gmail.com>
2025-04-25 00:11:55 +0800

    feat(scaffold): Optimize build.sh

--- radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/build.sh
+++ radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
-./mvnw clean -Dauto.layered.enabled=true -Pcoding,env-dev package || sh mvnw clean -Dauto.layered.enabled=true -Pcoding,env-dev package
+runtime_env=${1:-dev}
+
+active_profile="env-${runtime_env}"
+script_dir="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" && pwd)"
+project_root="${script_dir}"
+pushd "$script_dir" || exit 1
+pushd "$project_root" || exit 1
+echo "==>Building with env: ${runtime_env}"
+./mvnw clean -Dauto.layered.enabled=true -Pcoding,${active_profile} package || sh mvnw clean -Dauto.layered.enabled=true -Pcoding,${active_profile} package
+popd || exit 1
 docker build -f ./Dockerfile -t ${imageNamespace}/${appName} .
 docker push ${imageNamespace}/${appName}
+popd || exit 1

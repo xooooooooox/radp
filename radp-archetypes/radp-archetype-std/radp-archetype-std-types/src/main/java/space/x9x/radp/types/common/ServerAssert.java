b76de9398288b0f845eec506327702fe4898d1f7
xooooooooox <xozoz.sos@gmail.com>
2025-04-27 19:02:24 +0800

    fix: 优化 Assert

--- radp-archetypes/radp-archetype-std/radp-archetype-std-types/src/main/java/space/x9x/radp/types/common/ServerAssert.java
+++ radp-archetypes/radp-archetype-std/radp-archetype-std-types/src/main/java/space/x9x/radp/types/common/ServerAssert.java
@@ -1,22 +1,104 @@
 package space.x9x.radp.types.common;
 
+import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.PropertyKey;
 import space.x9x.radp.spring.framework.error.ErrorCode;
+import space.x9x.radp.spring.framework.error.ErrorCodeLoader;
 import space.x9x.radp.spring.framework.error.ServerException;
 import space.x9x.radp.spring.framework.error.asserts.BaseServerAssert;
-import lombok.experimental.UtilityClass;
+
+import java.util.Collection;
+import java.util.Map;
 
 /**
+ * Server assertion utility class that provides static methods for assertions.
+ *
  * @author x9x
  * @since 2024-10-24 21:54
  */
 @UtilityClass
-public class ServerAssert implements BaseServerAssert {
+public class ServerAssert {
 
+    private static final BaseServerAssert INSTANCE = new BaseServerAssert() {
+    };
+
+    /**
+     * Assert that the object is not null, throwing a ServerException with the given ErrorCode if it is.
+     *
+     * @param object    the object to check
+     * @param errorCode the error code to use in the exception
+     */
     public static void notNull(Object object, ErrorCode errorCode) {
         try {
             AssertUtils.notNull(object, errorCode);
         } catch (IllegalArgumentException e) {
             throw new ServerException(errorCode);
         }
+    }
+
+    public static void doesNotContain(String textToSearch, String substring,
+                                      @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode,
+                                      Object... placeholders) {
+        INSTANCE.doesNotContain(textToSearch, substring, errCode, placeholders);
+    }
+
+    public static void hasLength(String expression,
+                                 @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.hasLength(expression, errCode, placeholders);
+    }
+
+    public static void hasText(String text,
+                               @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.hasText(text, errCode, placeholders);
+    }
+
+    public static void isInstanceOf(Class<?> type, Object obj,
+                                    @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isInstanceOf(type, obj, errCode, placeholders);
+    }
+
+    public static void isNull(Object object,
+                              @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isNull(object, errCode, placeholders);
+    }
+
+    public static void notNull(Object object,
+                               @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notNull(object, errCode, placeholders);
+    }
+
+    public static void isTrue(boolean expression,
+                              @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isTrue(expression, errCode, placeholders);
+    }
+
+    public static void noNullElements(Collection<?> collection,
+                                      @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.noNullElements(collection, errCode, placeholders);
+    }
+
+    public static void notEmpty(Object[] array,
+                                @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notEmpty(array, errCode, placeholders);
+    }
+
+    public static void notEmpty(Collection<?> collection,
+                                @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notEmpty(collection, errCode, placeholders);
+    }
+
+    public static void notEmpty(Map<?, ?> map,
+                                @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notEmpty(map, errCode, placeholders);
+    }
+
+    public static void isAssignable(Class<?> superType, Class<?> subType,
+                                    @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isAssignable(superType, subType, errCode, placeholders);
+    }
+
+    public static void state(boolean expression,
+                             @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.state(expression, errCode, placeholders);
     }
 }
--- radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-types/src/main/java/types/common/ThirdServiceAssert.java
+++ radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-types/src/main/java/types/common/ThirdServiceAssert.java
@@ -3,13 +3,89 @@
 #set( $symbol_escape = '\' )
 package ${package}.types.common;
 
-import space.x9x.radp.spring.framework.error.asserts.BaseThirdServiceAssert;
 import lombok.experimental.UtilityClass;
+import org.jetbrains.annotations.PropertyKey;
+import space.x9x.radp.spring.framework.error.ErrorCodeLoader;
+import space.x9x.radp.spring.framework.error.asserts.BaseThirdServiceAssert;
+
+import java.util.Collection;
+import java.util.Map;
 
 /**
+ * Third-party service assertion utility class that provides static methods for assertions.
+ *
  * @author x9x
  * @since 2024-10-24 23:46
  */
 @UtilityClass
-public class ThirdServiceAssert implements BaseThirdServiceAssert {
+public class ThirdServiceAssert {
+
+    private static final BaseThirdServiceAssert INSTANCE = new BaseThirdServiceAssert() {
+    };
+
+    public static void doesNotContain(String textToSearch, String substring,
+                                      @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode,
+                                      Object... placeholders) {
+        INSTANCE.doesNotContain(textToSearch, substring, errCode, placeholders);
+    }
+
+    public static void hasLength(String expression,
+                                 @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.hasLength(expression, errCode, placeholders);
+    }
+
+    public static void hasText(String text,
+                               @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.hasText(text, errCode, placeholders);
+    }
+
+    public static void isInstanceOf(Class<?> type, Object obj,
+                                    @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isInstanceOf(type, obj, errCode, placeholders);
+    }
+
+    public static void isNull(Object object,
+                              @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isNull(object, errCode, placeholders);
+    }
+
+    public static void notNull(Object object,
+                               @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notNull(object, errCode, placeholders);
+    }
+
+    public static void isTrue(boolean expression,
+                              @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isTrue(expression, errCode, placeholders);
+    }
+
+    public static void noNullElements(Collection<?> collection,
+                                      @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.noNullElements(collection, errCode, placeholders);
+    }
+
+    public static void notEmpty(Object[] array,
+                                @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notEmpty(array, errCode, placeholders);
+    }
+
+    public static void notEmpty(Collection<?> collection,
+                                @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notEmpty(collection, errCode, placeholders);
+    }
+
+    public static void notEmpty(Map<?, ?> map,
+                                @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.notEmpty(map, errCode, placeholders);
+    }
+
+    public static void isAssignable(Class<?> superType, Class<?> subType,
+                                    @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.isAssignable(superType, subType, errCode, placeholders);
+    }
+
+    public static void state(boolean expression,
+                             @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
+        INSTANCE.state(expression, errCode, placeholders);
+    }
 }

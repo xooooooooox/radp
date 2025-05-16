39c95b0bfa9eb8d03722b49fa579b9464c43301e
xooooooooox <xozoz.sos@gmail.com>
2025-05-16 18:29:48 +0800

    refactor: move radp-archetype-xx to smoke-tests

---
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-simple/src/main/resources/db/migration/0.0.1/ddl/changes-ddl.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `demo`
+(
+    `id`     BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `name`   VARCHAR(20)     DEFAULT NULL COMMENT '姓名',
+    `locked` BIT(1) NOT NULL DEFAULT 0 COMMENT '是否锁定(1: 锁定, 0: 未锁定)',
+    PRIMARY KEY (`id`)
+) COMMENT = '示例表';

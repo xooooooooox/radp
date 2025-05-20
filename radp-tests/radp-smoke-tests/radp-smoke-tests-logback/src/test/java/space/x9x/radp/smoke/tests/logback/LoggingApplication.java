3ab45d3f5e48f8c32ec7da3040b2d195fcd24c7a
xooooooooox <xozoz.sos@gmail.com>
2025-05-21 01:25:28 +0800

    test(logging): Add smoke test module for Logback configuration
    Introduced a Logback-based smoke test module for validating logging configurations in the RADP framework. Includes a test application, Logback XML setup, and application YAML configuration to ensure proper integration and behavior.

---
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-logback/src/test/java/space/x9x/radp/smoke/tests/logback/LoggingApplication.java
@@ -0,0 +1,15 @@
+package space.x9x.radp.smoke.tests.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author x9x
+ * @since 2025-05-21 01:21
+ */
+@SpringBootApplication
+public class LoggingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingApplication.class, args);
+    }
+}

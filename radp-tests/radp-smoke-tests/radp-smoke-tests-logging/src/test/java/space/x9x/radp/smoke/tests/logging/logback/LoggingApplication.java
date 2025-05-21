7d0897cbb38d40c8a1144e62dc799265255cef35
xooooooooox <xozoz.sos@gmail.com>
2025-05-21 22:02:48 +0800

    tests: Add test for radp-logging

--- radp-tests/radp-smoke-tests/radp-smoke-tests-logging/src/test/java/space/x9x/radp/smoke/tests/logging/logback/LoggingApplication.java
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-logging/src/test/java/space/x9x/radp/smoke/tests/logging/logback/LoggingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author x9x
  * @since 2025-05-21 01:21
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "space.x9x.radp.smoke.tests.logging.logback.springboot")
 public class LoggingApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoggingApplication.class, args);

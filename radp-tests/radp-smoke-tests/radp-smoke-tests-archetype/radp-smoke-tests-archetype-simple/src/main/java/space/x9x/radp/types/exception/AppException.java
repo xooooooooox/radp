79b42815a61ab051d8948de2242342a0b159b893
xooooooooox <xozoz.sos@gmail.com>
2025-05-22 22:57:47 +0800

    refactor(asserts): relocate assert classes to a new package
    - Moved asserts from `types.common` to `types.exception.asserts`
    - Added license headers for consistency
    - Improved package organization for clarity and maintainability

---
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-simple/src/main/java/space/x9x/radp/types/exception/AppException.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package space.x9x.radp.types.exception;
+
+import org.jetbrains.annotations.PropertyKey;
+import space.x9x.radp.spring.framework.error.ErrorCode;
+import space.x9x.radp.spring.framework.error.ServerException;
+
+import java.io.Serial;
+
+/**
+ * @author x9x
+ * @since 2025-01-14 23:18
+ */
+public class AppException extends ServerException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public AppException(@PropertyKey(resourceBundle = "META-INF.error.message") String errCode, Throwable t) {
+        super(errCode, t);
+    }
+
+    public AppException(@PropertyKey(resourceBundle = "META-INF.error.message") String errCode, String messagePattern, Object... params) {
+        super(errCode, messagePattern, params);
+    }
+
+    public AppException(@PropertyKey(resourceBundle = "META-INF.error.message") String errCode, Object... params) {
+        super(errCode, params);
+    }
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public AppException(ErrorCode errorCode, Object... params) {
+        super(errorCode, params);
+    }
+}

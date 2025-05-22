9d2c45ee137a3574c7042966fb4069246f479549
xooooooooox <xozoz.sos@gmail.com>
2025-05-23 02:40:29 +0800

    chore(scaffold): Add RedisKeyProvider

---
+++ radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/infrastructure/redis/RedisKeyProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.infrastructure.redis;
+
+import space.x9x.radp.redis.spring.boot.constants.IRedisKeyProvider;
+
+/**
+ * @author x9x
+ * @since 2025-05-22 23:04
+ */
+public enum RedisKeyProvider implements IRedisKeyProvider {
+
+    INSTANCE;
+
+    private static final String PREFIX = "${appName}";
+
+    @Override
+    public String getPrefix() {
+        return PREFIX;
+    }
+}

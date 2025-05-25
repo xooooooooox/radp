7d46e116d8b141b4e0ca3beec5d9ff5e70ca22b9
xooooooooox <xozoz.sos@gmail.com>
2025-05-25 18:39:55 +0800

    chore(scaffold): Relocate assert classes to a new package

--- radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-types/src/main/java/types/exception/asserts/ThirdServiceAssert.java
+++ radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-types/src/main/java/types/exception/asserts/ThirdServiceAssert.java
@@ -1,12 +1,28 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.types.common;
+package ${package}.types.exception.asserts;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
-import space.x9x.radp.spring.framework.error.ClientException;
 import space.x9x.radp.spring.framework.error.ErrorCodeLoader;
+import space.x9x.radp.spring.framework.error.ThirdServiceException;
 import space.x9x.radp.spring.framework.error.asserts.BaseAssert;
 import space.x9x.radp.spring.framework.error.util.ExceptionUtils;
 
@@ -15,28 +31,27 @@ import java.util.Map;
 import java.util.function.BiFunction;
 
 /**
- * Client assertion utility class that provides static methods for assertions.
+ * Third-party service assertion utility class that provides static methods for assertions.
  *
  * @author x9x
  * @since 2024-10-24 23:46
  */
-public final class ClientAssert extends BaseAssert<ClientException> {
+public final class ThirdServiceAssert extends BaseAssert<ThirdServiceException> {
 
-    private ClientAssert() {
-
+    private ThirdServiceAssert() {
     }
 
-    private static final ClientAssert INSTANCE = new ClientAssert();
+    private static final ThirdServiceAssert INSTANCE = new ThirdServiceAssert();
 
 
     @Override
-    protected BiFunction<String, String, ClientException> getExceptionCreator() {
-        return ExceptionUtils::clientException;
+    protected BiFunction<String, String, ThirdServiceException> getExceptionCreator() {
+        return ExceptionUtils::thirdServiceException;
     }
 
     @Override
-    protected BiFunction<String, String, ClientException> getFormattedMessageExceptionCreator() {
-        return ExceptionUtils::clientExceptionWithFormattedMessage;
+    protected BiFunction<String, String, ThirdServiceException> getFormattedMessageExceptionCreator() {
+        return ExceptionUtils::thirdServiceExceptionWithFormattedMessage;
     }
 
     public static void doesNotContain(@NotNull String textToSearch, String substring,
@@ -55,7 +70,7 @@ public final class ClientAssert extends BaseAssert<ClientException> {
         INSTANCE.assertHasText(text, errCode, placeholders);
     }
 
-    public static void isInstanceOf(Class<?> type, Object obj,
+    public static void isInstanceOf(Class<?> type, @NotNull Object obj,
                                     @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
         INSTANCE.assertIsInstanceOf(type, obj, errCode, placeholders);
     }

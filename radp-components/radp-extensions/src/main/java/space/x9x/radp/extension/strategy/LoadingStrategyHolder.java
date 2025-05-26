0c9a096570153bc93655cade9bafa54bc1b5c106
xooooooooox <xozoz.sos@gmail.com>
2025-05-27 03:35:44 +0800

    chore(checkstyle): format code

--- radp-components/radp-extensions/src/main/java/space/x9x/radp/extension/strategy/LoadingStrategyHolder.java
+++ radp-components/radp-extensions/src/main/java/space/x9x/radp/extension/strategy/LoadingStrategyHolder.java
@@ -16,12 +16,12 @@
 
 package space.x9x.radp.extension.strategy;
 
-import space.x9x.radp.commons.lang.ArrayUtils;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.ServiceLoader;
 import java.util.stream.StreamSupport;
+
+import space.x9x.radp.commons.lang.ArrayUtils;
 
 /**
  * LoadingStrategy 容器
@@ -31,44 +31,42 @@ import java.util.stream.StreamSupport;
  */
 public class LoadingStrategyHolder {
 
-    /**
-     * 策略数组，存储所有加载策略
-     */
-    public static volatile LoadingStrategy[] strategies = loadLoadingStrategies();
+	/**
+	 * 策略数组，存储所有加载策略
+	 */
+	public static volatile LoadingStrategy[] strategies = loadLoadingStrategies();
 
-    /**
-     * Loads all available loading strategies using the Java ServiceLoader mechanism.
-     * This method discovers all implementations of the LoadingStrategy interface
-     * on the classpath, sorts them according to their priority, and returns them as an array.
-     *
-     * @return an array of discovered and sorted LoadingStrategy implementations
-     */
-    public static LoadingStrategy[] loadLoadingStrategies() {
-        return StreamSupport.stream(ServiceLoader.load(LoadingStrategy.class).spliterator(), false)
-                .sorted()
-                .toArray(LoadingStrategy[]::new);
-    }
+	/**
+	 * Loads all available loading strategies using the Java ServiceLoader mechanism. This
+	 * method discovers all implementations of the LoadingStrategy interface on the
+	 * classpath, sorts them according to their priority, and returns them as an array.
+	 * @return an array of discovered and sorted LoadingStrategy implementations
+	 */
+	public static LoadingStrategy[] loadLoadingStrategies() {
+		return StreamSupport.stream(ServiceLoader.load(LoadingStrategy.class).spliterator(), false)
+			.sorted()
+			.toArray(LoadingStrategy[]::new);
+	}
 
-    /**
-     * Gets the current list of loading strategies.
-     * This method returns the currently active loading strategies as an unmodifiable list.
-     *
-     * @return a list containing all currently active loading strategies
-     */
-    public static List<LoadingStrategy> getLoadingStrategies() {
-        return Arrays.asList(strategies);
-    }
+	/**
+	 * Gets the current list of loading strategies. This method returns the currently
+	 * active loading strategies as an unmodifiable list.
+	 * @return a list containing all currently active loading strategies
+	 */
+	public static List<LoadingStrategy> getLoadingStrategies() {
+		return Arrays.asList(strategies);
+	}
 
-    /**
-     * Sets the loading strategies to be used by the extension system.
-     * This method replaces the current set of strategies with the provided ones.
-     * If the provided array is empty, no change will be made.
-     *
-     * @param strategies the loading strategies to set
-     */
-    public static void setLoadingStrategies(LoadingStrategy... strategies) {
-        if (ArrayUtils.isNotEmpty(strategies)) {
-            LoadingStrategyHolder.strategies = strategies;
-        }
-    }
+	/**
+	 * Sets the loading strategies to be used by the extension system. This method
+	 * replaces the current set of strategies with the provided ones. If the provided
+	 * array is empty, no change will be made.
+	 * @param strategies the loading strategies to set
+	 */
+	public static void setLoadingStrategies(LoadingStrategy... strategies) {
+		if (ArrayUtils.isNotEmpty(strategies)) {
+			LoadingStrategyHolder.strategies = strategies;
+		}
+	}
+
 }

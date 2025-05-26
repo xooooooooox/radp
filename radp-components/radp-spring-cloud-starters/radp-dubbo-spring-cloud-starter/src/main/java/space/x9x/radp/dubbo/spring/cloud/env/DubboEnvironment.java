a27c3fca1ebe670e70ef6a2d72ee049150597cac
xooooooooox <xozoz.sos@gmail.com>
2025-05-26 13:43:44 +0800

    chore: Add Javadoc and format code

--- radp-components/radp-spring-cloud-starters/radp-dubbo-spring-cloud-starter/src/main/java/space/x9x/radp/dubbo/spring/cloud/env/DubboEnvironment.java
+++ radp-components/radp-spring-cloud-starters/radp-dubbo-spring-cloud-starter/src/main/java/space/x9x/radp/dubbo/spring/cloud/env/DubboEnvironment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.x9x.radp.dubbo.spring.cloud.env;
 
 import lombok.experimental.UtilityClass;
@@ -9,27 +25,29 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class DubboEnvironment {
     /**
-     * Property key for enabling or disabling Dubbo.
-     * This property controls whether Dubbo services are activated in the application.
+     * Property name for enabling/disabling Dubbo.
+     * This constant defines the property name used to control whether Dubbo is enabled.
      */
     public static final String ENABLED = "dubbo.enabled";
 
     /**
-     * Property key for the Dubbo protocol name.
-     * This property specifies which protocol (e.g., dubbo, rest, http) to use for RPC communication.
+     * Property name for the Dubbo protocol name configuration.
+     * This constant defines the property name used to configure the protocol
+     * that Dubbo will use for communication (e.g., dubbo, rest, http).
      */
     public static final String PROTOCOL = "dubbo.protocol.name";
 
     /**
-     * Property key for the Dubbo protocol port.
-     * This property specifies which port the Dubbo server will listen on.
+     * Property name for the Dubbo protocol port configuration.
+     * This constant defines the property name used to configure the port
+     * on which the Dubbo service will listen for incoming requests.
      */
     public static final String PORT = "dubbo.protocol.port";
 
     /**
-     * Property key for the Dubbo registry address.
-     * This property specifies the address of the service registry (e.g., ZooKeeper, Nacos)
-     * that Dubbo will use for service discovery.
+     * Property name for the Dubbo registry address configuration.
+     * This constant defines the property name used to configure the address
+     * of the registry service (e.g., ZooKeeper) that Dubbo will use for service discovery.
      */
     public static final String REGISTRY_ADDRESS = "dubbo.registry.address";
 }

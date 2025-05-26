85b02db0c495bafb21218a60dbf3feb167fb5b8d
xooooooooox <xozoz.sos@gmail.com>
2025-05-27 03:39:46 +0800

    chore(checkstyle): format code

--- radp-components/radp-extensions/src/main/java/space/x9x/radp/extension/strategy/InternalLoadingStrategy.java
+++ radp-components/radp-extensions/src/main/java/space/x9x/radp/extension/strategy/InternalLoadingStrategy.java
@@ -24,20 +24,21 @@ package space.x9x.radp.extension.strategy;
  */
 public class InternalLoadingStrategy implements LoadingStrategy {
 
-    /**
-     * Directory path for internal extensions.
-     * This constant defines the location where internal extension configuration files are stored.
-     * Internal extensions are loaded from this directory during the extension loading process.
-     */
-    public static final String META_INF = "META-INF/internal/";
+	/**
+	 * Directory path for internal extensions. This constant defines the location where
+	 * internal extension configuration files are stored. Internal extensions are loaded
+	 * from this directory during the extension loading process.
+	 */
+	public static final String META_INF = "META-INF/internal/";
 
-    @Override
-    public String directory() {
-        return META_INF;
-    }
+	@Override
+	public String directory() {
+		return META_INF;
+	}
 
-    @Override
-    public int getPriority() {
-        return MAX_PRIORITY;
-    }
+	@Override
+	public int getPriority() {
+		return MAX_PRIORITY;
+	}
+
 }

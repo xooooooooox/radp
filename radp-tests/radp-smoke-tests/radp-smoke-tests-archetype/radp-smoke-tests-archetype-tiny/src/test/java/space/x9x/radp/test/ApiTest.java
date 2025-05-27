9ee9d5a3dd5d44876235a8fb50673b1f5e142517
xooooooooox <xozoz.sos@gmail.com>
2025-05-28 03:48:51 +0800

    style(checkstyle): format code and add missing Javadoc

--- radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-tiny/src/test/java/space/x9x/radp/test/ApiTest.java
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-tiny/src/test/java/space/x9x/radp/test/ApiTest.java
@@ -16,9 +16,10 @@
 
 package space.x9x.radp.test;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * @author IO x9x
@@ -28,8 +29,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 class ApiTest {
 
-    @Test
-    void test() {
-        log.info("do something");
-    }
+	@Test
+	void test() {
+		log.info("do something");
+	}
+
 }

8263943b5ed3989071c70217286b3f7e94d15f6f
xooooooooox <xozoz.sos@gmail.com>
2025-05-28 03:51:38 +0800

    style(checkstyle): format code and add missing Javadoc

--- radp-tests/radp-smoke-tests/radp-smoke-tests-logging/src/test/java/space/x9x/radp/smoke/tests/logging/logback/classic/ClassicLogbackTest.java
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-logging/src/test/java/space/x9x/radp/smoke/tests/logging/logback/classic/ClassicLogbackTest.java
@@ -16,15 +16,19 @@
 
 package space.x9x.radp.smoke.tests.logging.logback.classic;
 
+import java.io.IOException;
+import java.net.URL;
+
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.net.URL;
 
 /**
  * Test for classic Logback configuration.
@@ -33,40 +37,41 @@ import java.net.URL;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ClassicLogbackTest {
 
-    private LoggerContext loggerCtx;
+	private LoggerContext loggerCtx;
 
-    @BeforeAll
-    void setup() throws IOException, JoranException {
-        loggerCtx = (LoggerContext) LoggerFactory.getILoggerFactory();
-        loggerCtx.reset();
+	@BeforeAll
+	void setup() throws IOException, JoranException {
+		loggerCtx = (LoggerContext) LoggerFactory.getILoggerFactory();
+		loggerCtx.reset();
 
-        URL configUrl = getClass().getClassLoader().getResources("logback-classic.xml").nextElement();
-        Assertions.assertNotNull(configUrl, "无法找到 logback-classic.xml");
-        JoranConfigurator cfg = new JoranConfigurator();
-        cfg.setContext(loggerCtx);
-        cfg.doConfigure(configUrl);
-    }
+		URL configUrl = getClass().getClassLoader().getResources("logback-classic.xml").nextElement();
+		Assertions.assertNotNull(configUrl, "无法找到 logback-classic.xml");
+		JoranConfigurator cfg = new JoranConfigurator();
+		cfg.setContext(loggerCtx);
+		cfg.doConfigure(configUrl);
+	}
 
-    @AfterAll
-    void tearDown() {
-        if (loggerCtx != null) {
-            loggerCtx.stop();
-        }
-    }
+	@AfterAll
+	void tearDown() {
+		if (loggerCtx != null) {
+			loggerCtx.stop();
+		}
+	}
 
-    @Test
-    void test_classicLogging() {
-        // Log messages at different levels
-        log.trace("This is a TRACE message");
-        log.debug("This is a DEBUG message");
-        log.info("This is an INFO message");
-        log.warn("This is a WARN message");
-        log.error("This is an ERROR message");
-        log.warn("This is a WARN message with exception", new IllegalArgumentException("Something went wrong"));
-        log.error("This is a ERROR message with exception", new IllegalArgumentException("Something went wrong"));
+	@Test
+	void test_classicLogging() {
+		// Log messages at different levels
+		log.trace("This is a TRACE message");
+		log.debug("This is a DEBUG message");
+		log.info("This is an INFO message");
+		log.warn("This is a WARN message");
+		log.error("This is an ERROR message");
+		log.warn("This is a WARN message with exception", new IllegalArgumentException("Something went wrong"));
+		log.error("This is a ERROR message with exception", new IllegalArgumentException("Something went wrong"));
 
-        // Verify the log file was created at the expected path with the expected filename
+		// Verify the log file was created at the expected path with the expected filename
 
-        // Verify the log content as expected
-    }
+		// Verify the log content as expected
+	}
+
 }

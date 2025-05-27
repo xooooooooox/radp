c695f0f9c6fc2b2fb519dafe07d5db1e0da0ecf2
xooooooooox <xozoz.sos@gmail.com>
2025-05-28 02:14:44 +0800

    style(checkstyle): format code and add missing Javadoc

--- radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-simple/src/main/java/space/x9x/radp/types/exception/asserts/ThirdServiceAssert.java
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-simple/src/main/java/space/x9x/radp/types/exception/asserts/ThirdServiceAssert.java
@@ -37,81 +37,206 @@ import space.x9x.radp.spring.framework.error.util.ExceptionUtils;
  */
 public final class ThirdServiceAssert extends BaseAssert<ThirdServiceException> {
 
+	/**
+	 * private constructor to prevent instantiation.
+	 */
 	private ThirdServiceAssert() {
 	}
 
+	/**
+	 * singleton instance of the ThirdServiceAssert class.
+	 */
 	private static final ThirdServiceAssert INSTANCE = new ThirdServiceAssert();
 
+	/**
+	 * returns a function that creates a ThirdServiceException with the specified error
+	 * code and message.
+	 * @return a function that creates a ThirdServiceException
+	 */
 	@Override
 	protected BiFunction<String, String, ThirdServiceException> getExceptionCreator() {
 		return ExceptionUtils::thirdServiceException;
 	}
 
+	/**
+	 * returns a function that creates a ThirdServiceException with the specified error
+	 * code and formatted message.
+	 * @return a function that creates a ThirdServiceException with a formatted message
+	 */
 	@Override
 	protected BiFunction<String, String, ThirdServiceException> getFormattedMessageExceptionCreator() {
 		return ExceptionUtils::thirdServiceExceptionWithFormattedMessage;
 	}
 
+	/**
+	 * asserts that the specified string does not contain the specified substring,
+	 * throwing a ThirdServiceException with the specified error code if it does.
+	 * @param textToSearch the string to search
+	 * @param substring the substring to search for
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the string contains the substring
+	 */
 	public static void doesNotContain(@NotNull String textToSearch, String substring,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertDoesNotContain(textToSearch, substring, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified string has a length greater than 0, throwing a
+	 * ThirdServiceException with the specified error code if it doesn't.
+	 * @param expression the string to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the string has a length of 0
+	 */
 	public static void hasLength(@NotNull String expression,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertHasLength(expression, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified string has text (contains at least one non-whitespace
+	 * character), throwing a ThirdServiceException with the specified error code if it
+	 * doesn't.
+	 * @param text the string to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the string does not contain the text
+	 */
 	public static void hasText(String text, @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode,
 			Object... placeholders) {
 		INSTANCE.assertHasText(text, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified object is an instance of the specified type, throwing a
+	 * ThirdServiceException with the specified error code if it isn't.
+	 * @param type the type to check against
+	 * @param obj the object to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the object is not an instance of the specified
+	 * type
+	 */
 	public static void isInstanceOf(Class<?> type, @NotNull Object obj,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertIsInstanceOf(type, obj, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified object is null, throwing a ThirdServiceException with
+	 * the specified error code if it isn't.
+	 * @param object the object to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the object is not null
+	 */
 	public static void isNull(Object object, @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode,
 			Object... placeholders) {
 		INSTANCE.assertIsNull(object, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified object is not null, throwing a ThirdServiceException
+	 * with the specified error code if it is.
+	 * @param object the object to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the object is null
+	 */
 	public static void notNull(Object object, @PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode,
 			Object... placeholders) {
 		INSTANCE.assertNotNull(object, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified expression is true, throwing a ThirdServiceException
+	 * with the specified error code if it isn't.
+	 * @param expression the boolean expression to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the expression is false
+	 */
 	public static void isTrue(boolean expression,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertIsTrue(expression, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified collection contains no null elements, throwing a
+	 * ThirdServiceException with the specified error code if it does.
+	 * @param collection the collection to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the collection contains a null element
+	 */
 	public static void noNullElements(@NotNull Collection<?> collection,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertNoNullElements(collection, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified array is not empty, throwing a ThirdServiceException
+	 * with the specified error code if it is.
+	 * @param array the array to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the array is empty
+	 */
 	public static void notEmpty(Object[] array,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertNotEmpty(array, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified collection is not empty, throwing a
+	 * ThirdServiceException with the specified error code if it is.
+	 * @param collection the collection to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the collection is empty
+	 */
 	public static void notEmpty(@NotNull Collection<?> collection,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertNotEmpty(collection, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified map is not empty, throwing a ThirdServiceException with
+	 * the specified error code if it is.
+	 * @param map the map to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the map is empty
+	 */
 	public static void notEmpty(@NotNull Map<?, ?> map,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertNotEmpty(map, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified subType is assignable to the specified superType,
+	 * throwing a ThirdServiceException with the specified error code if it isn't.
+	 * @param superType the super type to check against
+	 * @param subType the subtype to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the subType is not assignable to the superType
+	 */
 	public static void isAssignable(Class<?> superType, @NotNull Class<?> subType,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertIsAssignable(superType, subType, errCode, placeholders);
 	}
 
+	/**
+	 * asserts that the specified expression is true, representing a valid state, throwing
+	 * a ThirdServiceException with the specified error code if it isn't.
+	 * @param expression the boolean expression to check
+	 * @param errCode the error code to use in the exception
+	 * @param placeholders the placeholders to use in the error message
+	 * @throws ThirdServiceException if the expression is false
+	 */
 	public static void state(boolean expression,
 			@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... placeholders) {
 		INSTANCE.assertState(expression, errCode, placeholders);

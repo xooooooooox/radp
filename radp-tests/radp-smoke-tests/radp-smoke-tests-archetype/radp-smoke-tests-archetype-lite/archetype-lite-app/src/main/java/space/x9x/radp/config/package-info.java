78ea03f6989f5b646db7744b40f66cf1955dcb72
xooooooooox <xozoz.sos@gmail.com>
2025-05-28 03:24:28 +0800

    style(checkstyle): format code and add missing Javadoc

--- radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-lite/archetype-lite-app/src/main/java/space/x9x/radp/config/package-info.java
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-lite/archetype-lite-app/src/main/java/space/x9x/radp/config/package-info.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2012-2025 the original author or authors.
  *
@@ -15,8 +16,6 @@
  */
 
 /**
- * 1. 用于管理引入的Jar所需的资源启动或者初始化处理
- * 2. 如果有AOP切面，可以再建一个aop包，来写切面逻辑
+ * 1. 用于管理引入的Jar所需的资源启动或者初始化处理 2. 如果有AOP切面，可以再建一个aop包，来写切面逻辑
  */
 package space.x9x.radp.config;
-
--- radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-lite/archetype-lite-types/src/main/java/space/x9x/radp/types/exception/AppException.java
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-lite/archetype-lite-types/src/main/java/space/x9x/radp/types/exception/AppException.java
@@ -22,8 +22,8 @@ import space.x9x.radp.spring.framework.error.ErrorCode;
 import space.x9x.radp.spring.framework.error.ServerException;
 
 /**
- * Application-specific exception class that extends the ServerException.
- * This exception is used to represent application-level errors with specific error codes.
+ * Application-specific exception class that extends the ServerException. This exception
+ * is used to represent application-level errors with specific error codes.
  *
  * @author IO x9x
  * @since 2025-01-14 23:18
@@ -34,7 +34,6 @@ public class AppException extends ServerException {
 
 	/**
 	 * Constructs an AppException with the specified error code and cause.
-	 *
 	 * @param errCode the error code from the resource bundle
 	 * @param t the cause of this exception
 	 */
@@ -43,8 +42,8 @@ public class AppException extends ServerException {
 	}
 
 	/**
-	 * Constructs an AppException with the specified error code, message pattern, and parameters.
-	 *
+	 * Constructs an AppException with the specified error code, message pattern, and
+	 * parameters.
 	 * @param errCode the error code from the resource bundle
 	 * @param messagePattern the pattern for the error message
 	 * @param params the parameters to be used in the message pattern
@@ -56,7 +55,6 @@ public class AppException extends ServerException {
 
 	/**
 	 * Constructs an AppException with the specified error code and parameters.
-	 *
 	 * @param errCode the error code from the resource bundle
 	 * @param params the parameters to be used in the error message
 	 */
@@ -66,7 +64,6 @@ public class AppException extends ServerException {
 
 	/**
 	 * Constructs an AppException with the specified error code object.
-	 *
 	 * @param errorCode the error code object containing code and message information
 	 */
 	public AppException(ErrorCode errorCode) {
@@ -75,7 +72,6 @@ public class AppException extends ServerException {
 
 	/**
 	 * Constructs an AppException with the specified error code object and parameters.
-	 *
 	 * @param errorCode the error code object containing code and message information
 	 * @param params the parameters to be used in the error message
 	 */

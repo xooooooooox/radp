b03c210c262d7e572ce88a451bea4f9cff75a412
xooooooooox <xozoz.sos@gmail.com>
2025-05-28 03:19:42 +0800

    docs(scaffold): Add missing javadoc

--- radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/src/main/java/Application.java
+++ radp-archetypes/scaffold-tiny/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
@@ -7,12 +23,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * Main application class for the RADP Simple Archetype. This class serves as the entry
+ * point for the Spring Boot application.
+ *
+ * The {@code @SpringBootApplication} annotation enables auto-configuration and component
+ * scanning. The {@code @EnableRestExceptionHandler} annotation enables global REST
+ * exception handling.
+ *
  * @author IO x9x
- * @since 2024-11-15 11:03
+ * @since 2024-10-11 16:33
  */
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+
+	/**
+	 * Main method which serves as the entry point for the application.
+	 * @param args command line arguments passed to the application
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
 }

437c6ba94b7e0fdec9806bd8149a4ea4027e7560
xooooooooox <xozoz.sos@gmail.com>
2025-05-29 00:03:06 +0800

    chore(checkstyle): format code and resolve checkstyle error

--- radp-components/radp-spring-framework/src/main/java/space/x9x/radp/spring/framework/error/ErrorCode.java
+++ radp-components/radp-spring-framework/src/main/java/space/x9x/radp/spring/framework/error/ErrorCode.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.x9x.radp.spring.framework.error;
 
 import lombok.AllArgsConstructor;
@@ -5,6 +21,11 @@ import lombok.Data;
 import org.jetbrains.annotations.PropertyKey;
 
 /**
+ * Represents an error code and its associated message for standardized error handling.
+ * This class encapsulates error information that can be used throughout the application
+ * to provide consistent error responses. It supports loading error messages from resource
+ * bundles and formatting them with parameters.
+ *
  * @author IO x9x
  * @since 2024-10-24 10:31
  */
@@ -12,19 +33,29 @@ import org.jetbrains.annotations.PropertyKey;
 @AllArgsConstructor
 public class ErrorCode {
 
-    private final String code;
-    private final String message;
+	/**
+	 * The unique identifier for this error code. This code is used to look up error
+	 * messages and identify specific error conditions.
+	 */
+	private final String code;
 
-    /**
-     * Constructs a new ErrorCode with the specified error code and parameters.
-     * The error message is loaded from the resource bundle using the error code,
-     * and the parameters are used for message formatting.
-     *
-     * @param errCode the error code that identifies the error message in the resource bundle
-     * @param params  the parameters to be substituted in the error message
-     */
-    public ErrorCode(@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... params) {
-        this.code = errCode;
-        this.message = ErrorCodeLoader.getErrMessage(errCode, params);
-    }
+	/**
+	 * The human-readable error message associated with this error code. This message can
+	 * be displayed to users or logged for troubleshooting.
+	 */
+	private final String message;
+
+	/**
+	 * Constructs a new ErrorCode with the specified error code and parameters. The error
+	 * message is loaded from the resource bundle using the error code, and the parameters
+	 * are used for message formatting.
+	 * @param errCode the error code that identifies the error message in the resource
+	 * bundle
+	 * @param params the parameters to be substituted in the error message
+	 */
+	public ErrorCode(@PropertyKey(resourceBundle = ErrorCodeLoader.BUNDLE_NAME) String errCode, Object... params) {
+		this.code = errCode;
+		this.message = ErrorCodeLoader.getErrMessage(errCode, params);
+	}
+
 }
--- radp-components/radp-spring-framework/src/main/java/space/x9x/radp/spring/framework/web/rest/config/ApiConfig.java
+++ radp-components/radp-spring-framework/src/main/java/space/x9x/radp/spring/framework/web/rest/config/ApiConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.x9x.radp.spring.framework.web.rest.config;
 
 import lombok.EqualsAndHashCode;
@@ -6,6 +22,9 @@ import lombok.Setter;
 import lombok.ToString;
 
 /**
+ * Configuration class for API settings, providing properties to configure RESTful API
+ * behavior.
+ *
  * @author IO x9x
  * @since 2024-09-30 23:20
  */
@@ -14,9 +33,10 @@ import lombok.ToString;
 @Setter
 @Getter
 public class ApiConfig {
-    /**
-     * API 前缀
-     * 实现所有 Controller 提供的 RESTFul API 的统一前缀
-     */
-    private String prefix;
+
+	/**
+	 * API 前缀 实现所有 Controller 提供的 RESTFul API 的统一前缀.
+	 */
+	private String prefix;
+
 }

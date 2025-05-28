570f628d2d8105b1e531c84c76abee46b4345fdb
xooooooooox <xozoz.sos@gmail.com>
2025-05-28 20:41:05 +0800

    chore(checkstyle): format code and resolve checkstyle error

--- radp-components/radp-design-pattern-framework/compound-patterns/radp-decision-tree-pattern/src/main/java/space/x9x/radp/design/framework/tree/AbstractStrategyRouter.java
+++ radp-components/radp-design-pattern-framework/compound-patterns/radp-decision-tree-pattern/src/main/java/space/x9x/radp/design/framework/tree/AbstractStrategyRouter.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.x9x.radp.design.framework.tree;
 
 /**
@@ -8,29 +24,27 @@ package space.x9x.radp.design.framework.tree;
  */
 public abstract class AbstractStrategyRouter<T, D, R> implements StrategyMapper<T, D, R>, StrategyHandler<T, D, R> {
 
-    /**
-     * 默认策略处理器，当没有找到匹配的策略处理器时使用
-     * 默认使用空实现，返回null
-     */
-    @SuppressWarnings("unchecked")
-    protected StrategyHandler<T, D, R> defaultStrategyHandler = StrategyHandler.EMPTY;
+	/**
+	 * 默认策略处理器. 当没有找到匹配的策略处理器时使用 默认使用空实现，返回null
+	 */
+	@SuppressWarnings("unchecked")
+	protected StrategyHandler<T, D, R> defaultStrategyHandler = StrategyHandler.EMPTY;
 
-    /**
-     * 路由方法，根据请求参数和动态上下文选择合适的策略处理器
-     * 如果找不到匹配的策略处理器，则使用默认策略处理器
-     *
-     * @param requestParameter 请求参数
-     * @param dynamicContext   动态上下文
-     * @return 策略处理结果
-     * @throws Exception 当策略处理过程中发生异常时抛出
-     */
-    public R router(T requestParameter, D dynamicContext) throws Exception {
-        // 通过调用 策略映射器StrategyHandler#get, 控制节点流程走向
-        StrategyHandler<T, D, R> nextStrategyHandler = get(requestParameter, dynamicContext);
+	/**
+	 * 路由方法. 根据请求参数和动态上下文选择合适的策略处理器 如果找不到匹配的策略处理器，则使用默认策略处理器
+	 * @param requestParameter 请求参数
+	 * @param dynamicContext 动态上下文
+	 * @return 策略处理结果
+	 * @throws Exception 当策略处理过程中发生异常时抛出
+	 */
+	public R router(T requestParameter, D dynamicContext) throws Exception {
+		// 通过调用 策略映射器StrategyHandler#get, 控制节点流程走向
+		StrategyHandler<T, D, R> nextStrategyHandler = get(requestParameter, dynamicContext);
 
-        if (nextStrategyHandler != null) {
-            return nextStrategyHandler.apply(requestParameter, dynamicContext);
-        }
-        return defaultStrategyHandler.apply(requestParameter, dynamicContext);
-    }
+		if (nextStrategyHandler != null) {
+			return nextStrategyHandler.apply(requestParameter, dynamicContext);
+		}
+		return defaultStrategyHandler.apply(requestParameter, dynamicContext);
+	}
+
 }
--- radp-components/radp-design-pattern-framework/compound-patterns/radp-decision-tree-pattern/src/main/java/space/x9x/radp/design/framework/tree/StrategyMapper.java
+++ radp-components/radp-design-pattern-framework/compound-patterns/radp-decision-tree-pattern/src/main/java/space/x9x/radp/design/framework/tree/StrategyMapper.java
@@ -1,25 +1,41 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.x9x.radp.design.framework.tree;
 
 /**
- * 策略映射器
+ * 策略映射器.
  *
+ * @author IO x9x
+ * @since 2025-01-14 14:25
  * @param <T> 入参
  * @param <D> 动态上下文
  * @param <R> 返参
- * @author IO x9x
- * @since 2025-01-14 14:25
  */
 public interface StrategyMapper<T, D, R> {
 
-    /**
-     * 获取待执行策略
-     * <p>
-     * 用于获取每个要执行的节点, 相当于一个流程走完进入到下一个流程的过程.
-     *
-     * @param requestParameter 入参
-     * @param dynamicContext   动态上下文
-     * @return 下一个待执行策略
-     * @throws Exception 异常
-     */
-    StrategyHandler<T, D, R> get(T requestParameter, D dynamicContext) throws Exception;
+	/**
+	 * 获取待执行策略.
+	 * <p>
+	 * 用于获取每个要执行的节点, 相当于一个流程走完进入到下一个流程的过程.
+	 * @param requestParameter 入参
+	 * @param dynamicContext 动态上下文
+	 * @return 下一个待执行策略
+	 * @throws Exception 异常
+	 */
+	StrategyHandler<T, D, R> get(T requestParameter, D dynamicContext) throws Exception;
+
 }

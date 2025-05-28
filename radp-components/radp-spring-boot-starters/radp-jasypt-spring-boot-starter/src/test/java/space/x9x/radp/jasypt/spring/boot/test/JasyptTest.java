303a2056ec9b3f92b2a30f04e0349ec5b6026c3e
xooooooooox <xozoz.sos@gmail.com>
2025-05-28 22:00:26 +0800

    chore(checkstyle): format code and resolve checkstyle error

--- radp-components/radp-spring-boot-starters/radp-jasypt-spring-boot-starter/src/test/java/space/x9x/radp/jasypt/spring/boot/test/JasyptTest.java
+++ radp-components/radp-spring-boot-starters/radp-jasypt-spring-boot-starter/src/test/java/space/x9x/radp/jasypt/spring/boot/test/JasyptTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package space.x9x.radp.jasypt.spring.boot.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -5,11 +21,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.StringEncryptor;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import space.x9x.radp.jasypt.spring.boot.TestApplication;
 import space.x9x.radp.jasypt.spring.boot.util.JasyptUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author IO x9x
@@ -19,21 +36,23 @@ import space.x9x.radp.jasypt.spring.boot.util.JasyptUtils;
 @Slf4j
 class JasyptTest {
 
-    @Autowired
-    private StringEncryptor stringEncryptor;
+	@Autowired
+	private StringEncryptor stringEncryptor;
 
-    @Test
-    void test() {
-        String[] plainTexts = {"root", "123456"};
-        for (String plainText : plainTexts) {
-            // 使用当前 SpringBoot 上下文已经集成的 jasypt 加密器进行加密
-            String encrypted1 = JasyptUtils.encrypt(stringEncryptor, plainText);
-            // 使用当前 SpringBoot 上下文已经集成的 jasypt 加密器进行解密
-            Assertions.assertEquals(plainText, JasyptUtils.decrypt(stringEncryptor, encrypted1));
-            // 使用指定的 jasypt 加密器进行加密
-            String encrypted2 = JasyptUtils.customPBEEncrypt(plainText, "PBEWithMD5AndDES", "dsaf#,jds.klfj1");
-            // 使用指定的 jasypt 加密器进行解密
-            Assertions.assertEquals(plainText, JasyptUtils.customPBEDecrypt(encrypted2, "PBEWithMD5AndDES", "dsaf#,jds.klfj1"));
-        }
-    }
+	@Test
+	void test() {
+		String[] plainTexts = { "root", "123456" };
+		for (String plainText : plainTexts) {
+			// 使用当前 SpringBoot 上下文已经集成的 jasypt 加密器进行加密
+			String encrypted1 = JasyptUtils.encrypt(stringEncryptor, plainText);
+			// 使用当前 SpringBoot 上下文已经集成的 jasypt 加密器进行解密
+			assertThat(plainText).isEqualTo(JasyptUtils.decrypt(stringEncryptor, encrypted1));
+			// 使用指定的 jasypt 加密器进行加密
+			String encrypted2 = JasyptUtils.customPBEEncrypt(plainText, "PBEWithMD5AndDES", "dsaf#,jds.klfj1");
+			// 使用指定的 jasypt 加密器进行解密
+			assertThat(plainText)
+				.isEqualTo(JasyptUtils.customPBEDecrypt(encrypted2, "PBEWithMD5AndDES", "dsaf#,jds.klfj1"));
+		}
+	}
+
 }

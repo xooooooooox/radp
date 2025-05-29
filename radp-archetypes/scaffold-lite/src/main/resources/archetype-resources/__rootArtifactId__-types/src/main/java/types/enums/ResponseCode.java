855a83bb0e2f5330bddb4ee82d2829ada3e84bc4
xooooooooox <xozoz.sos@gmail.com>
2025-05-29 15:19:47 +0800

    chore(scaffold): format code and resolve checkstyle error

--- radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-types/src/main/java/types/enums/ResponseCode.java
+++ radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-types/src/main/java/types/enums/ResponseCode.java
@@ -17,6 +17,23 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
+
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ${package}.types.enums;
 
 import lombok.Getter;
@@ -27,8 +44,8 @@ import space.x9x.radp.spring.framework.error.ErrorCodeLoader;
 import space.x9x.radp.spring.framework.error.GlobalResponseCode;
 
 /**
- * Enumeration of business response codes.
- * This enum provides standardized response codes for business operations.
+ * Enumeration of business response codes. This enum provides standardized response codes
+ * for business operations.
  *
  * @author IO x9x
  * @since 2024-10-24 14:08

ce96fcf6d6d6f8058914256a637b3e44526887f1
xooooooooox <xozoz.sos@gmail.com>
2025-05-29 15:56:04 +0800

    chore(scaffold): format code and resolve checkstyle error

--- radp-archetypes/scaffold-simple/src/main/resources/archetype-resources/src/main/java/Application.java
+++ radp-archetypes/scaffold-simple/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -1,20 +1,37 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 package ${package};
 
-import space.x9x.radp.spring.framework.web.rest.annotation.EnableRestExceptionHandler;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author IO x9x
- * @since 2024-10-11 16:33
- */
+import space.x9x.radp.spring.framework.web.rest.annotation.EnableRestExceptionHandler;
+
 @SpringBootApplication
+@Configurable
 @EnableRestExceptionHandler
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class);
+	}
+
 }

87260a484e6ac97615b7bbd194d000d3f12598a1
xooooooooox <xozoz.sos@gmail.com>
2025-05-29 15:45:38 +0800

    chore(scaffold): format code and resolve checkstyle error

--- radp-archetypes/scaffold-simple/src/main/resources/archetype-resources/src/main/java/app/config/ThreadPoolConfigProperties.java
+++ radp-archetypes/scaffold-simple/src/main/resources/archetype-resources/src/main/java/app/config/ThreadPoolConfigProperties.java
@@ -1,68 +1,92 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 package ${package}.app.config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
 import space.x9x.radp.spring.framework.bootstrap.constant.Globals;
 
 /**
+ * Configuration properties for thread pool executor settings. This class defines the
+ * properties that can be configured in the application properties file to customize the
+ * thread pool executor behavior.
+ *
  * @author IO x9x
  * @since 2025-01-17 11:13
  */
 @Data
-@ConfigurationProperties(prefix = Globals.RADP_CONFIGURATION_PROPERTIES_PREFIX + "thread.pool.executor.config", ignoreInvalidFields = true)
+@ConfigurationProperties(prefix = Globals.RADP_CONFIGURATION_PROPERTIES_PREFIX + "thread.pool.executor.config",
+		ignoreInvalidFields = true)
 public class ThreadPoolConfigProperties {
 
-    /**
-     * 核心线程数
-     */
-    private Integer corePoolSize = 20;
+	/**
+	 * Core pool size for the thread pool executor.
+	 */
+	private Integer corePoolSize = 20;
 
-    /**
-     * 最大线程数
-     */
-    private Integer maxPoolSize = 200;
+	/**
+	 * Maximum pool size for the thread pool executor.
+	 */
+	private Integer maxPoolSize = 200;
 
-    /**
-     * 最大等待时间（单位：秒）
-     */
-    private Long keepAliveTime = 10L;
+	/**
+	 * Maximum time (in seconds) that excess idle threads will wait for new tasks.
+	 */
+	private Long keepAliveTime = 10L;
 
-    /**
-     * 最大队列数
-     */
-    private Integer blockQueueSize = 5000;
+	/**
+	 * Maximum queue size for the thread pool executor.
+	 */
+	private Integer blockQueueSize = 5000;
 
-    /**
-     * 拒绝策略
-     */
-    private Policy policy = Policy.ABORT_POLICY;
+	/**
+	 * Rejection policy for the thread pool executor.
+	 */
+	private Policy policy = Policy.ABORT_POLICY;
 
+	public enum Policy {
 
-    public enum Policy {
+		/**
+		 * Discards the task and throws a
+		 * {@link java.util.concurrent.RejectedExecutionException}.
+		 */
+		ABORT_POLICY,
 
-        /**
-         * 丢弃任务并抛出 {@link java.util.concurrent.RejectedExecutionException} 异常
-         */
-        ABORT_POLICY,
+		/**
+		 * Discards the task silently without throwing an exception.
+		 */
+		DISCARD_POLICY,
 
-        /**
-         * 直接丢弃任务,但不会抛出异常
-         */
-        DISCARD_POLICY,
+		/**
+		 * Discards the oldest task in the queue and then tries to add the new task again.
+		 */
+		DISCARD_OLDEST_POLICY,
 
-        /**
-         * 将最早进入队列的任务删除,之后再尝试加入队列的任务被拒绝
-         */
-        DISCARD_OLDEST_POLICY,
+		/**
+		 * If the task cannot be added to the thread pool, the calling thread executes the
+		 * task itself.
+		 */
+		CALLER_RUNS_POLICY
 
-        /**
-         * 如果任务添加线程池失败,那么主线程自己执行该任务
-         */
-        CALLER_RUNS_POLICY,
+	}
 
-        ;
-    }
 }

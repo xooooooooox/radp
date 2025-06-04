30fba8e6cff2ed5770db471c76af59a9501972c8
xooooooooox <xozoz.sos@gmail.com>
2025-06-04 22:57:50 +0800

    chore(scaffold): format code and resolve checkstyle error

--- radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/infrastructure/dao/convertor/package-info.java
+++ radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/infrastructure/dao/convertor/package-info.java
@@ -2,7 +2,8 @@
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 /**
- * 1. 在这里定义 PO {@literal <->} DomainObject 转换器 2. 命名 {@code IXxxPOConvertor} 3. 示例 <pre><code>
+ * 1. 在这里定义 PO {@literal <->} DomainObject 转换器 2. 命名 {@code IXxxPOConvertor} 3. 示例
+ * <pre><code>
  * import org.mapstruct.Mapper;
  *
  * &#64;Mapper(componentModel = "spring")

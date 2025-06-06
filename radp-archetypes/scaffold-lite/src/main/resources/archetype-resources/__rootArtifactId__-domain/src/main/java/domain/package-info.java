10dd0d6e746cf4a3a45e0bb61b2feba750cac4b4
xooooooooox <xozoz.sos@gmail.com>
2025-06-07 00:57:35 +0800

    chore(scaffold): add package-info.java templates for domain, infrastructure, types, and trigger layers in scaffold-lite archetype

---
+++ radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-domain/src/main/java/domain/package-info.java
@@ -0,0 +1,11 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Domain layer package containing the core business logic and domain models.
+ * <p>
+ * This package contains all the domain-specific logic, entities, value objects,
+ * aggregates, domain services, and domain events that represent the business concepts
+ * and rules of the application.
+ */
+package ${package}.domain;
---
+++ radp-archetypes/scaffold-lite/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/infrastructure/redis/package-info.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Redis infrastructure package.
+ * <p>
+ * This package contains components for interacting with Redis, including: - Redis client
+ * configurations - Redis data access objects - Redis-specific utilities and helpers
+ * <p>
+ * These components provide caching and data storage capabilities using Redis.
+ */
+package ${package}.infrastructure.redis;

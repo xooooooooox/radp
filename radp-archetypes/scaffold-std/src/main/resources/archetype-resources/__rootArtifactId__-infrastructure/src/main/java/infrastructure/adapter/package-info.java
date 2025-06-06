ae2eff0b82cdd468cecdb7d071966d792ee4ac82
xooooooooox <xozoz.sos@gmail.com>
2025-06-07 01:03:53 +0800

    chore(scaffold): add package-info.java templates for domain, infrastructure, types, and trigger layers in scaffold-lite archetype

---
+++ radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-infrastructure/src/main/java/infrastructure/adapter/package-info.java
@@ -0,0 +1,11 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Infrastructure adapter package.
+ * <p>
+ * This package contains adapter implementations that connect the domain layer to external
+ * systems and infrastructure services. These adapters implement the port interfaces
+ * defined in the domain layer.
+ */
+package ${package}.infrastructure.adapter;
---
+++ radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-types/src/main/java/types/package-info.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/**
+ * Types package containing common types used across the application.
+ *
+ * This package includes: - Enumerations - Event definitions - Exception types - Common
+ * value objects
+ *
+ * These types are shared across different layers of the application and provide a common
+ * vocabulary for the domain.
+ */
+package ${package}.types;

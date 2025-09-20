0a3083029193d936a346cb035edf0c693db530c0
xooooooooox <xozoz.sos@gmail.com>
2025-09-20 16:57:04 +0800

    docs(spring-data): add Javadoc for `DataSourceResolver` interface and `resolveDataSource` method

--- radp-components/radp-spring-data/src/main/java/space/x9x/radp/spring/data/jdbc/datasource/DataSourceResolver.java
+++ radp-components/radp-spring-data/src/main/java/space/x9x/radp/spring/data/jdbc/datasource/DataSourceResolver.java
@@ -21,12 +21,19 @@ import javax.sql.DataSource;
 import space.x9x.radp.extension.SPI;
 
 /**
+ * 数据源解析器
+ *
  * @author IO x9x
  * @since 2024-09-30 13:57
  */
 @SPI
 public interface DataSourceResolver {
 
+	/**
+	 * 解析数据源
+	 * @param originalDataSource 原始数据源
+	 * @return 解析后的数据源
+	 */
 	DataSource resolveDataSource(DataSource originalDataSource);
 
 }

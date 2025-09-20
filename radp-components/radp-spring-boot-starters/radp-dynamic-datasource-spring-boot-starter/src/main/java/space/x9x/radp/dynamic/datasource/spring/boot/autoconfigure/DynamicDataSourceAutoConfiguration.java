d855067b9c52986520d66dc3df938ed3e1a34c94
xooooooooox <xozoz.sos@gmail.com>
2025-09-20 19:36:08 +0800

    refactor(radp-mybatis): rename configuration classes and update property fields
    - Renamed `RadpMybatis*AutoConfiguration` classes for consistency.
    - Updated default property field names in `MybatisPlusExtensionProperties`.
    - Improved documentation with additional field details and references.

---
+++ radp-components/radp-spring-boot-starters/radp-dynamic-datasource-spring-boot-starter/src/main/java/space/x9x/radp/dynamic/datasource/spring/boot/autoconfigure/DynamicDataSourceAutoConfiguration.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package space.x9x.radp.dynamic.datasource.spring.boot.autoconfigure;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.baomidou.dynamic.datasource.aop.DynamicDataSourceAnnotationAdvisor;
+import com.baomidou.dynamic.datasource.processor.DsProcessor;
+import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDatasourceAopProperties;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.aop.Advisor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import space.x9x.radp.dynamic.datasource.spring.boot.custom.CustomDynamicDataSourceAnnotationInterceptor;
+import space.x9x.radp.dynamic.datasource.spring.boot.custom.CustomDynamicRoutingDataSource;
+import space.x9x.radp.spring.boot.bootstrap.constants.Conditions;
+
+/**
+ * @author IO x9x
+ * @since 2025-09-20 00:48
+ */
+
+@Slf4j
+@ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX, name = Conditions.ENABLED,
+		havingValue = Conditions.ENABLED, matchIfMissing = true)
+@ConditionalOnClass(DynamicDataSourceProvider.class)
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@AutoConfiguration
+public class DynamicDataSourceAutoConfiguration {
+
+	private static final String AUTOWIRED_DYNAMIC_ROUTING_DATA_SOURCE = "Autowired DynamicDataSourceDataSource";
+
+	private static final String AUTOWIRED_DYNAMIC_DATASOURCE_ANNOTATION_ADVISOR = "Autowired DynamicDataSourceAnnotationAdvisor";
+
+	private final DynamicDataSourceProperties properties;
+
+	public DynamicDataSourceAutoConfiguration(DynamicDataSourceProperties properties) {
+		this.properties = properties;
+	}
+
+	@Primary
+	@Bean
+	public DataSource dataSource(List<DynamicDataSourceProvider> providers) {
+		log.info(AUTOWIRED_DYNAMIC_ROUTING_DATA_SOURCE);
+		DynamicRoutingDataSource dataSource = new CustomDynamicRoutingDataSource(providers);
+		dataSource.setPrimary(properties.getPrimary());
+		dataSource.setStrict(properties.getStrict());
+		dataSource.setStrategy(properties.getStrategy());
+		dataSource.setP6spy(properties.getP6spy());
+		dataSource.setSeata(properties.getSeata());
+		return dataSource;
+	}
+
+	@ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX + ".aop", name = Conditions.ENABLED,
+			havingValue = Conditions.TRUE, matchIfMissing = true)
+	@Primary
+	@Bean
+	public Advisor dynamicDatasourceAnnotationAdvisor(DsProcessor dsProcessor) {
+		log.debug(AUTOWIRED_DYNAMIC_DATASOURCE_ANNOTATION_ADVISOR);
+		DynamicDatasourceAopProperties aopProperties = properties.getAop();
+		CustomDynamicDataSourceAnnotationInterceptor interceptor = new CustomDynamicDataSourceAnnotationInterceptor(
+				aopProperties.getAllowedPublicOnly(), dsProcessor);
+		DynamicDataSourceAnnotationAdvisor advisor = new DynamicDataSourceAnnotationAdvisor(interceptor, DS.class);
+		advisor.setOrder(aopProperties.getOrder());
+		return advisor;
+	}
+
+}
---
+++ radp-components/radp-spring-boot-starters/radp-dynamic-datasource-spring-boot-starter/src/main/java/space/x9x/radp/dynamic/datasource/spring/boot/custom/CustomDynamicRoutingDataSource.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package space.x9x.radp.dynamic.datasource.spring.boot.custom;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+
+import space.x9x.radp.spring.data.jdbc.datasource.routing.RoutingDataSourceContextHolder;
+
+/**
+ * @author IO x9x
+ * @since 2025-09-20 01:04
+ */
+public class CustomDynamicRoutingDataSource extends DynamicRoutingDataSource {
+
+	public CustomDynamicRoutingDataSource(List<DynamicDataSourceProvider> providers) {
+		super(providers);
+	}
+
+	@Override
+	public DataSource determineDataSource() {
+		String dsKey = RoutingDataSourceContextHolder.peek();
+		return getDataSource(dsKey);
+	}
+
+}

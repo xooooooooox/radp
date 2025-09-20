7064ef80c95fdbcfee7c16715ac7c55e665c3cee
xooooooooox <xozoz.sos@gmail.com>
2025-09-20 22:19:52 +0800

    style(spring-data): add missing periods in `DataSourceResolver` JavaDoc

--- radp-components/radp-spring-data/src/main/java/space/x9x/radp/spring/data/jdbc/datasource/DataSourceResolver.java
+++ radp-components/radp-spring-data/src/main/java/space/x9x/radp/spring/data/jdbc/datasource/DataSourceResolver.java
@@ -21,7 +21,7 @@ import javax.sql.DataSource;
 import space.x9x.radp.extension.SPI;
 
 /**
- * 数据源解析器
+ * 数据源解析器.
  *
  * @author IO x9x
  * @since 2024-09-30 13:57
@@ -30,7 +30,7 @@ import space.x9x.radp.extension.SPI;
 public interface DataSourceResolver {
 
 	/**
-	 * 解析数据源
+	 * 解析数据源.
 	 * @param originalDataSource 原始数据源
 	 * @return 解析后的数据源
 	 */

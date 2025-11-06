c17a9d4e9638064517499d9e2c87970cd7018486
xooooooooox <xozoz.sos@gmail.com>
2025-11-07 00:59:43 +0800

    feat(excel): introduce `DictConvert` and `DictFormat` for dictionary-based data conversion
    Add `DictConvert` for handling Excel data dictionary conversions and `DictFormat` annotation for field-level dictionary formatting.

---
+++ radp-components/radp-solutions/radp-solution-excel/src/main/java/space/x9x/radp/solutions/excel/conver/DictConvert.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package space.x9x.radp.solutions.excel.conver;
+
+import cn.idev.excel.converters.Converter;
+import cn.idev.excel.enums.CellDataTypeEnum;
+import cn.idev.excel.metadata.GlobalConfiguration;
+import cn.idev.excel.metadata.data.ReadCellData;
+import cn.idev.excel.metadata.data.WriteCellData;
+import cn.idev.excel.metadata.property.ExcelContentProperty;
+import lombok.extern.slf4j.Slf4j;
+
+import space.x9x.radp.solutions.excel.annotations.DictFormat;
+
+/**
+ * Excel 数据字典转换器.
+ * @author IO x9x
+ * @since 2025-11-07 00:10
+ */
+@Slf4j
+// TODO v2.26-2025/11/7: 待实现
+public class DictConvert implements Converter<Object> {
+
+	@Override
+	public Class<?> supportJavaTypeKey() {
+		throw new UnsupportedOperationException("暂不支持,也不需要");
+	}
+
+	@Override
+	public CellDataTypeEnum supportExcelTypeKey() {
+		throw new UnsupportedOperationException("暂不支持,也不需要");
+	}
+
+	@Override
+	public Object convertToJavaData(ReadCellData<?> readCellData, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+		// 使用字典解析
+		String type = getType(contentProperty);
+		String label = readCellData.getStringValue();
+		return null;
+	}
+
+	@Override
+	public WriteCellData<?> convertToExcelData(Object value, ExcelContentProperty contentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+		return null;
+	}
+
+	private static String getType(ExcelContentProperty contentProperty) {
+		return contentProperty.getField().getAnnotation(DictFormat.class).value();
+	}
+}

92c206ef77544dd67124c110e3a369765c22632b
xooooooooox <xozoz.sos@gmail.com>
2025-11-22 21:03:37 +0800

    feat(scaffold): Restructure package organization in the xxx-api layer.

--- radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/dto/package-info.java
+++ radp-archetypes/scaffold-std/src/main/resources/archetype-resources/__rootArtifactId__-api/src/main/java/api/dto/package-info.java
@@ -2,8 +2,11 @@
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
 /**
- * 在这里定义 RPC 接口的入参和出参对象 format: <br/>
- * 1. {@code XxxRequestDTO} <br/>
- * 2. {@code XxxResponseDTO} <br/>
+ * 在这里定义 API 接口的入参和出参对象(遵循CQRS) format: <br/>
+ * <ul>
+ *     <li>入参(写模型): {@code XxxCmd}</li>
+ *     <li>入参(读模型): {@code XxxQry}</li>
+ *     <li>出参: {@code XxxDTO}</li>
+ * </ul>
  */
 package ${package}.api.dto;

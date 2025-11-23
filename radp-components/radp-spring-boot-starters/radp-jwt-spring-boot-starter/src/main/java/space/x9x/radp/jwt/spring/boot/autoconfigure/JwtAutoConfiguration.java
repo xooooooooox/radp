e261371f8e3b9d5736ce044bc7cfd4e7cb40b2de
xooooooooox <xozoz.sos@gmail.com>
2025-11-23 13:45:51 +0800

    feat(security): Implement JWT-based authentication and authorization
    - Introduced `LoginUserDetails` for user authentication details.
    - Added utilities such as `SpringSecurityUtils` to manage security contexts.
    - Developed core JWT components: `JwtTokenProvider`, `JwtTokenStore`, and `JwtTokenService`.
    - Created `JwtAuthorizationFilter` and `JwtSecurityConfigurer` for authentication filtering and configuration.
    - Defined token models including `AccessToken` and `RefreshToken`.

---
+++ radp-components/radp-spring-boot-starters/radp-jwt-spring-boot-starter/src/main/java/space/x9x/radp/jwt/spring/boot/autoconfigure/JwtAutoConfiguration.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2012-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package space.x9x.radp.jwt.spring.boot.autoconfigure;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import space.x9x.radp.jwt.spring.boot.env.JwtProperties;
+import space.x9x.radp.spring.boot.bootstrap.constants.Conditions;
+import space.x9x.radp.spring.security.jwt.config.JwtConfig;
+import space.x9x.radp.spring.security.jwt.token.JwtTokenProvider;
+import space.x9x.radp.spring.security.jwt.token.JwtTokenService;
+import space.x9x.radp.spring.security.jwt.token.JwtTokenStore;
+
+/**
+ * @author x9x
+ * @since 2025-11-23 02:15
+ */
+@AutoConfiguration
+@EnableConfigurationProperties(JwtProperties.class)
+@ConditionalOnProperty(prefix = JwtProperties.PREFIX, name = Conditions.ENABLED, havingValue = Conditions.TRUE)
+@RequiredArgsConstructor
+public class JwtAutoConfiguration {
+
+	private final JwtProperties jwtProperties;
+
+	@ConditionalOnMissingBean
+	@Bean
+	public JwtTokenProvider jwtTokenProvider(JwtTokenStore jwtTokenStore) {
+		JwtConfig jwtConfig = JwtConfig.builder()
+			.header(this.jwtProperties.getConfig().getHeader())
+			.secret(this.jwtProperties.getConfig().getSecret())
+			.base64Secret(this.jwtProperties.getConfig().getBase64Secret())
+			.tokenValidityInSeconds(this.jwtProperties.getConfig().getTokenValidityInSeconds())
+			.tokenValidityInSecondsForRememberMe(
+					this.jwtProperties.getConfig().getTokenValidityInSecondsForRememberMe())
+			.anonymousUrls(this.jwtProperties.getConfig().getAnonymousUrls())
+			.authenticatedUrls(this.jwtProperties.getConfig().getAuthenticatedUrls())
+			.permitAllUrls(this.jwtProperties.getConfig().getPermitAllUrls())
+			.build();
+		return new JwtTokenProvider(jwtConfig, jwtTokenStore);
+	}
+
+	@ConditionalOnMissingBean
+	@Bean
+	public JwtTokenService jwtTokenService(AuthenticationManager authenticationManager,
+			JwtTokenProvider jwtTokenProvider) {
+		return new JwtTokenService(authenticationManager, jwtTokenProvider);
+	}
+
+}

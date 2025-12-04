b3b97ff2ee6d38835d31a8c42e8f7c5c0ea534c9
xooooooooox <xozoz.sos@gmail.com>
2025-12-04 14:03:01 +0800

    refactor(radp-common): refactor ArrayUtil

--- radp-components/radp-commons/src/main/java/space/x9x/radp/commons/lang/ArrayUtil.java
+++ radp-components/radp-commons/src/main/java/space/x9x/radp/commons/lang/ArrayUtil.java
@@ -17,6 +17,7 @@
 package space.x9x.radp.commons.lang;
 
 import lombok.experimental.UtilityClass;
+import org.apache.commons.lang3.ArrayUtils;
 
 /**
  * Utility class for array operations. This class extends Apache Commons Lang's ArrayUtils
@@ -27,6 +28,126 @@ import lombok.experimental.UtilityClass;
  * @since 2024-09-24 23:10
  */
 @UtilityClass
-public class ArrayUtil extends org.apache.commons.lang3.ArrayUtils {
+public class ArrayUtil {
+
+	public static boolean isEmpty(final boolean[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final byte[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final char[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final double[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final float[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final int[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final long[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final Object[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isEmpty(final short[] array) {
+		return ArrayUtils.isEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final boolean[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final byte[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final char[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final double[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final float[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final int[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final long[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean isNotEmpty(final short[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static <T> boolean isNotEmpty(final T[] array) {
+		return ArrayUtils.isNotEmpty(array);
+	}
+
+	public static boolean contains(final boolean[] array, final boolean valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static boolean contains(final byte[] array, final byte valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static boolean contains(final char[] array, final char valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static boolean contains(final double[] array, final double valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static boolean contains(final double[] array, final double valueToFind, final double tolerance) {
+		return ArrayUtils.contains(array, valueToFind, tolerance);
+	}
+
+	public static boolean contains(final float[] array, final float valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static boolean contains(final int[] array, final int valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static boolean contains(final long[] array, final long valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static boolean contains(final Object[] array, final Object objectToFind) {
+		return ArrayUtils.contains(array, objectToFind);
+	}
+
+	public static boolean contains(final short[] array, final short valueToFind) {
+		return ArrayUtils.contains(array, valueToFind);
+	}
+
+	public static <T> T get(final T[] array, final int index) {
+		return ArrayUtils.get(array, index);
+	}
+
+	public static <T> T get(final T[] array, final int index, final T defaultValue) {
+		return ArrayUtils.get(array, index, defaultValue);
+	}
 
 }

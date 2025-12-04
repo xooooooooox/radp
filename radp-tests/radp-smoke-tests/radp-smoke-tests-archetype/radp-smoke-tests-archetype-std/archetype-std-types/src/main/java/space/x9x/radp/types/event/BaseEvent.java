318df4dc2d880af594c8ae2c45652dd01bdf1a27
xooooooooox <xozoz.sos@gmail.com>
2025-12-04 23:25:02 +0800

    style: fix (javadoc) AtclauseOrder: Block tags have to appear in the order '[@param, @author, @since, @see, @version, @serial, @deprecated]'.

--- radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-std/archetype-std-types/src/main/java/space/x9x/radp/types/event/BaseEvent.java
+++ radp-tests/radp-smoke-tests/radp-smoke-tests-archetype/radp-smoke-tests-archetype-std/archetype-std-types/src/main/java/space/x9x/radp/types/event/BaseEvent.java
@@ -27,9 +27,9 @@ import lombok.NoArgsConstructor;
  * Defines the structure for message queue (MQ) messages. This abstract class provides a
  * base for all event types in the system.
  *
+ * @param <T> the type of data contained in the event
  * @author RADP x9x
  * @since 2024-11-28 14:43
- * @param <T> the type of data contained in the event
  */
 @Data
 public abstract class BaseEvent<T> {
